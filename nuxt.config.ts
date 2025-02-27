06ff1d40064480f3b08982e2460b2bdf529ce389
wai-lin <dev.wailin@gmail.com>
2025-02-27 11:44:28 +0700

    chore: app config baseUrl and set vscode default formatter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxt/icon'],
+	appConfig: {
+		betterAuth: {
+			baseUrl: process.env.BETTER_AUTH_URL ?? 'http://localhost:3000',
+		},
+	},
 	app: {
 		head: {
 			title: 'MiniWaffle',
